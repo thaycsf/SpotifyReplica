7835c7f3cb79d1138ea4140010aedad63081be38
Thayna Cristina Da Silva Ferreira <thayna.ferreira@juntossomosmais.com.br>
2025-02-17 14:50:27 -0300

    fix: app return

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
-const App = () => {
+const App = () => (
   <>
     <Header />
     <Main />
-  </>;
-};
+  </>
+);
 
 export default App;
